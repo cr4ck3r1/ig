a99f3559c90dd5d1bfe6aea79098672307be1f5e
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2020-12-12 15:43:46 +0300

    Update ig.py

--- ig.py
+++ ig.py
@@ -9,7 +9,7 @@ import sys
 #EMIL HENA TAWAW
 # FA7S INSTA TAWAW
 def ya():
-    file=open("punjabi.txt","w")
+    file=open("yahoo.txt","w")
     os.system('clear')
     name=input("NAME :")
     url=int(input("CHAND EMIL BE :"))
@@ -28,7 +28,7 @@ def ya():
 
 
 def gm():
-    file=open("punjabi.txt","w")
+    file=open("gmail.txt","w")
     os.system('clear')
     name=input("NAME :")
     url=int(input("CHAND EMIL BE :"))
@@ -51,7 +51,7 @@ def gm():
 
 
 def ho():
-    file=open("punjabi.txt","w")
+    file=open("hotmail.txt","w")
     os.system('clear')
     name=input("NAME :")
     url=int(input("CHAND EMIL BE :"))
@@ -97,7 +97,7 @@ def chek2():
 
 
 def aol():
-    file=open("punjabi.txt","w")
+    file=open("aol.txt","w")
     os.system('clear')
     name=input("NAME :")
     url=int(input("CHAND EMIL BE :"))
